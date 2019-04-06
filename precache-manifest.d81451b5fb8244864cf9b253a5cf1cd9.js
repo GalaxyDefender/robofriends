bd7dc4c22bd5d704088dc27b228002ec16eba6de
Quentin <qgerard.gerard@gmail.com>
2019-04-06 18:48:16 +0200

    Updates

---
+++ precache-manifest.d81451b5fb8244864cf9b253a5cf1cd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "88c3ed6975f280d658aa6c42d9527b72",
+    "url": "/robofriends/static/media/BITSUMIS.88c3ed69.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "7bf58076807d6b1fecf3",
+    "url": "/robofriends/static/js/main.af0af4fc.chunk.js"
+  },
+  {
+    "revision": "20a2f3c3782404bcbfb9",
+    "url": "/robofriends/static/js/2.a68475e0.chunk.js"
+  },
+  {
+    "revision": "7bf58076807d6b1fecf3",
+    "url": "/robofriends/static/css/main.8e4d87d4.chunk.css"
+  },
+  {
+    "revision": "20a2f3c3782404bcbfb9",
+    "url": "/robofriends/static/css/2.a4b7c782.chunk.css"
+  },
+  {
+    "revision": "e6aaefe6d45021528e6b3e5b0df1bd83",
+    "url": "/robofriends/index.html"
+  }
+];
